cc174b67c12290a7757b44029d9db75a8d158cd3
Hanlong Yang <peteryang363@gmail.com>
2026-05-02 14:40:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("ISOM5240")
 st.header("Header")
-st.subheader(Subheader)
+st.subheader("Subheader")
 st.write("ISOM5240")
 
 st.write("Hello, *World!* :sunglasses:")
